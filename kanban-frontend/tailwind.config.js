0c7517745e76615cfd090ec58a3bf771c6704ee0
Ckola99 <Christopherkola@gmail.com>
2024-11-11 11:19:47 +0200

    added fonts and custom colours into tailwind

--- kanban-frontend/tailwind.config.js
+++ kanban-frontend/tailwind.config.js
@@ -5,7 +5,24 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        'plus-jakarta': ['"Plus Jakarta Sans"', 'sans-serif'],
+      },
+      colors: {
+        'primary-blue': "#635FC7",
+        'secondary-blue': "#ABA4FF",
+        'primary-black': "#000112",
+        'secondary-black': "#20212C",
+        'primary-gray': "#2B2C37",
+        'secondary-gray': "#3E3F4E",
+        "tertiary-gray": "#828FA3",
+        "primary-light-gray": "#E4EBFA",
+        "secondary-light-gray": "#F4F7FD",
+        "red": "EA5555",
+        "secondary-red": "#FF9898"
+      },
+    },
   },
   plugins: [],
 }
